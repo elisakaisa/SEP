ceb61106983aff2a02f655d32e857394e153533a
elisakaisa <elisa.k.perini@gmail.com>
2022-09-30 10:49:39 +0200

    name and role are stored to shred prefs so we don't alwaxys have to login

--- app/src/main/java/com/example/sep/ActivityLogin.java
+++ app/src/main/java/com/example/sep/ActivityLogin.java
@@ -3,17 +3,23 @@ package com.example.sep;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.example.sep.model.Employee;
 import com.example.sep.viewModel.LoginViewModel;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 
 public class ActivityLogin extends AppCompatActivity {
+
+    public static SharedPreferences sharedPref; // needs to be static for the same preferences to be accessed form other classes
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +43,16 @@ public class ActivityLogin extends AppCompatActivity {
         // Re-created activities receive the same MyViewModel instance created by the first activity.
         LoginViewModel loginVM = new ViewModelProvider(this).get(LoginViewModel.class);
 
+        /* ----- USER FROM STORAGE ------*/
+        sharedPref = getPreferences(Context.MODE_PRIVATE);
+        Employee userFromStorage = new Employee(null, null, null);
+        if (sharedPref.contains("name")) {
+            Log.i("login", sharedPref.getString("name", null));
+            userFromStorage.setName(sharedPref.getString("name", null));
+            userFromStorage.setRole(sharedPref.getString("role", null));
+            startActivityAfterLogin(userFromStorage.getName(), userFromStorage.getRole());
+        }
+
         /*--------- listeners ---------*/
         login.setOnClickListener(v -> {
             if (!isFieldEmpty(String.valueOf(etPassword.getText()))) {
@@ -52,10 +68,14 @@ public class ActivityLogin extends AppCompatActivity {
 
         loginVM.setLoginListener((loggedIn, errorMessage, user) -> {
             if (loggedIn) {
-                Intent intent = new Intent(this, BaseActivity.class);
-                intent.putExtra("name", user.getName());
-                intent.putExtra("role", user.getRole());
-                startActivity(intent);
+
+                // save the logged in user into local storage so no need to relog in every time the app starts
+                SharedPreferences.Editor prefsEditor = sharedPref.edit();
+                prefsEditor.putString("name", user.getName());
+                prefsEditor.putString("role", user.getRole());
+                prefsEditor.apply();
+
+                startActivityAfterLogin(user.getName(), user.getRole());
             } else {
                 Toast.makeText(getApplicationContext(), errorMessage, Toast.LENGTH_SHORT).show();
                 if (errorMessage.equals("Wrong password")) tiPassword.setError("Wrong password");
@@ -81,5 +101,12 @@ public class ActivityLogin extends AppCompatActivity {
     private Boolean isFieldEmpty(String text) {
         // for error messages in text fields
         return text != null && text.length() > 1;
+    }
+
+    private void startActivityAfterLogin(String name, String role) {
+        Intent intent = new Intent(this, BaseActivity.class);
+        intent.putExtra("name", name);
+        intent.putExtra("role", role);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/sep/BaseActivity.java
+++ app/src/main/java/com/example/sep/BaseActivity.java
@@ -3,14 +3,19 @@ package com.example.sep;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
+import com.example.sep.viewModel.LoginViewModel;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class BaseActivity extends AppCompatActivity {
@@ -27,7 +32,7 @@ public class BaseActivity extends AppCompatActivity {
         bottomNavigationView.setOnItemSelectedListener(this::listener);
         TextView tv_username = findViewById(R.id.tv_logged_as);
         TextView tv_role = findViewById(R.id.tv_role);
-        ImageButton btnLogin = findViewById(R.id.img_btn_login);
+        ImageButton btnLogout = findViewById(R.id.img_btn_login);
 
         // default fragment
         // TODO: set default fragment depending on user role
@@ -44,8 +49,23 @@ public class BaseActivity extends AppCompatActivity {
         tv_username.setText(s);
         tv_role.setText(role);
 
+        /*--------- view models ---------*/
+        // Create a ViewModel the first time the system calls an activity's onCreate() method.
+        // Re-created activities receive the same MyViewModel instance created by the first activity.
+        LoginViewModel loginVM = new ViewModelProvider(this).get(LoginViewModel.class);
+
         /*---------- LISTENERS ----------*/
-          btnLogin.setOnClickListener(v -> startActivity(new Intent(this, ActivityLogin.class)));
+        btnLogout.setOnClickListener(v -> {
+
+            // remove user from local storage
+            ActivityLogin.sharedPref.edit().remove("name").apply();
+            ActivityLogin.sharedPref.edit().remove("role").apply();
+
+            loginVM.deleteUser();
+
+            startActivity(new Intent(this, ActivityLogin.class));
+            finish();
+        });
 
     }
 
